57efe55b3981635dc70109b19f88792f664836af
Arun Pottekat <apottekat@gmail.com>
2016-04-22 12:36:22 -0700

    Create show action and add gravatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :articles
 	before_save { self.email = email.downcase }
+
+	include Gravtastic
+	gravtastic
+
 	validates :username, presence: true, uniqueness: { case_sensitive: false }, 
 	length: {minimum: 3, maximum: 25}
 
