634b752c880bffe1c4cd9573a4dd2eac32ad3186
Arun Pottekat <apottekat@gmail.com>
2016-04-22 11:05:57 -0700

    Add secure password and attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422162503) do
+ActiveRecord::Schema.define(version: 20160422175613) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20160422162503) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 255
-    t.string   "email",      limit: 255
+    t.string   "username",        limit: 255
+    t.string   "email",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest", limit: 255
   end
 
 end
