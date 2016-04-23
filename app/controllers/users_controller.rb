4298851ad9641a0ca7d19636e85908979e4cdf76
Arun Pottekat <apottekat@gmail.com>
2016-04-23 08:40:31 -0700

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to Touristen #{@user.username}"
-			redirect_to articles_path
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
 		else
 			render "new"
 		end
