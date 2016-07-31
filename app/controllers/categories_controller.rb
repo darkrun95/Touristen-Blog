50c9d1d8fb4c5c3fdea3846d06f7f348597f276f
Arun Pottekat <apottekat@gmail.com>
2016-07-31 19:18:11 +0530

    Some random commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+class CategoriesController < ApplicationController
+
+	def index
+
+	end
+
+	def new
+
+	end
+
+	def show
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -17,6 +18,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
